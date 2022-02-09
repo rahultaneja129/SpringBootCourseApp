4eedb367bdf6b46d87bd4a2b5d080d6b3a993c7b
Rahul <rahultaneja129@gmail.com>
2022-02-09 10:25:02 +0530

    code

--- src/main/java/com/appdata/courseApp/CourseAppApplication.java
+++ src/main/java/com/appdata/courseApp/CourseAppApplication.java
@@ -2,10 +2,16 @@ package com.appdata.courseApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseAppApplication {
+public class CourseAppApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(CourseAppApplication.class);
+	} 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseAppApplication.class, args);
 	}
